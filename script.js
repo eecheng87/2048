093ef9f3f934d38ccf215645a690b4a88e58a7cd
eecheng <yucheng871011@gmail.com>
2018-08-16 00:15:12 +0800

    complete menu

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('play_btn').addEventListener('click', function() {
+    document.getElementsByClassName('menu')[0].style.display = "none";
+    startGame();
+});
+
+function startGame() {
+
+}
